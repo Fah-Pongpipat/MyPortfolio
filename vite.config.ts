c32f96dd273200c13f78973e33770a54977f9030
Pongpipat Siangsai <163412313+Fah-Pongpipat@users.noreply.github.com>
2025-05-02 01:35:08 +0700

    Edited code

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MyPortfolio/', // <-- ต้องตรงกับชื่อ repo บน GitHub
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
